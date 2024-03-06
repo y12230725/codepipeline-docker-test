a4e622f80ae16416dbde927dd1e1bc9ef82bedbf
MZC01-CXZ5309 <cxz5309@mz.co.kr>
2024-03-06 15:31:03 +0900

    Fix: 파일 경로 수정

--- scripts/process/after-install-process.sh
+++ scripts/process/after-install-process.sh
@@ -15,8 +15,8 @@ fi
 export RABBIT_MQ_URI=amqp://$(docker inspect -f {{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}} rabbitmq):5672
 
 # --env-file 문제로 env 문자 변경
-sed -i "s/'//g" /home/ec2-user/app/.env.development
-sed -i 's/"//g' /home/ec2-user/app/.env.development
+sed -i "s/'//g" /home/ec2-user/app/hello-app/.env.development
+sed -i 's/"//g' /home/ec2-user/app/hello-app/.env.development
 
 # Creating and starting a docker container using a new image
-docker run -i -t -d -p 9000:9000 --name hello-app --env-file /home/ec2-user/app/.env.development --env RABBIT_MQ_URI=$RABBIT_MQ_URI --env NODE_ENV=development --network hello-network hello-app:latest
+docker run -i -t -d -p 9000:9000 --name hello-app --env-file /home/ec2-user/app/hello-app/.env.development --env RABBIT_MQ_URI=$RABBIT_MQ_URI --env NODE_ENV=development --network hello-network hello-app:latest
